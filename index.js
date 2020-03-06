9887c0ba268e92d7b68f77c2349a0e6641e9b24d
svensura <svensura@germanscreens.de>
2020-03-06 18:05:54 +0100

    GPIO integrated

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const {app, BrowserWindow } = require('electron')
 
+
 let window
 
 const createWindow = () => {
 
-    window = new BrowserWindow ({width: 800, height : 600})
+    window = new BrowserWindow ({
+        width: 800, 
+        height : 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+
+    })
     window.loadURL(`file://${__dirname}/index.html`)
+    window.webContents.openDevTools()
 
 } 
 
 app.on('ready',createWindow)
+
